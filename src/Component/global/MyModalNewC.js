9009158642d2e9bbd0ca4f26a0feac7edfc32550
zemrlab <zoraida.emperatriz@gmail.com>
2018-10-29 02:23:34 -0500

    Update MyModalNewC.js

--- src/Component/global/MyModalNewC.js
+++ src/Component/global/MyModalNewC.js
@@ -6,47 +6,131 @@ import './css/bootstrap.css';
 import './css/NewC.css';
 import './css/ListarComprobanteNewC.css';
 class MyModal extends Component{
+
     constructor(){
         super();
         this.handlerGuardar=this.handlerGuardar.bind(this);
+        this.handleRevisar=this.handleRevisar.bind(this);
         // this.texto=React.createRef();
     }
-      componentWillMount(){
+
+    componentWillMount(){
         let data;
         const url = URL.url.concat('conceptos');
-       // const url= 'https://api-modulocontrol.herokuapp.com/conceptos';
+        const url1 = URL.url.concat('ubicaciones');
+        const url2 = URL.url.concat('tipos');        
         fetch(url,{
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }          
+        }).then(res => res.json())
+        .then(res => {
+            if (res.status) { // exito
+                //  console.log(res);
+                data=res;
+                //console.log(data);
+                // Llenar select de conceptos
+                var x = document.getElementById("concepto");
+                for (var i = 0; i < data["data"].length; i++) {
+                    var miOption=document.createElement("option");
+                    miOption.text = data["data"][i]["concepto"];
+                    miOption.setAttribute("value",data["data"][i]["id_concepto"]);
+                    x.add(miOption);
+                }
+            }else{
+                alert("Fallo al cargar datos, Intentelo mas tarde");
+            }
+        });
+
+        fetch(url1,{
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            }
+            //body: JSON.stringify(data)
+        }).then(res => res.json())
+        .then(res => {
+            if (res.status){ // exito
+                //  console.log(res);
+                data=res;
+                //console.log(data);
+                // Llenar select de conceptos
+                var x = document.getElementById("ubicacion");
+                for (var i = 0; i < data["data"].length; i++) {
+                    var miOption=document.createElement("option");
+                    miOption.text = data["data"][i]["descripcion"];
+                    miOption.setAttribute("value",data["data"][i]["id_ubicacion"]);
+                    x.add(miOption);
+                }
+            }
+            else{
+            alert("Fallo al cargar datos, Intentelo mas tarde");
+            }
+        });
+
+        fetch(url2,{
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
               }
             //body: JSON.stringify(data)
-                    })
-            .then(res => res.json())
-            .then(res => {
-                if (res.status) { // exito
-                  //  console.log(res);
-                    data=res;
-                    //console.log(data);
-                    // Llenar select de conceptos
-                    var x = document.getElementById("concepto");
-                    for (var i = 0; i < data["data"].length; i++) {
-                      var miOption=document.createElement("option");
-                      miOption.text = data["data"][i]["concepto"];
-                      miOption.setAttribute("value",data["data"][i]["id_concepto"]);
-                      x.add(miOption);
-                    }
-                }else{
+        }).then(res => res.json())
+        .then(res => {
+            if (res.status) { // exito
+              //  console.log(res);
+                data=res;
+                //console.log(data);
+                // Llenar select de conceptos
+                var x = document.getElementById("tipo");
+                for (var i = 0; i < data["data"].length; i++) {
+                  var miOption=document.createElement("option");
+                  miOption.text = data["data"][i]["descripcion"];
+                  miOption.setAttribute("value",data["data"][i]["id_tipo"]);
+                  x.add(miOption);
+                }
+            }else{
                 alert("Fallo al cargar datos, Intentelo mas tarde")
-              }
-          });
+            }
+        });
+    }
+
+    handleRevisar(){
+        //WEBADAS
+        var data2 = {};
+        data2.numero = document.getElementById("recibo").value;
+
+        const url2 = URL.url.concat('verificar');
+
+        fetch(url2,{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data2)
+        })
+        .then(res => res.json())
+        .then(res => {
+
+            if(res.data[0].verificar === false){
+                console.log('Entro al response remaster');
+                alert('El numero de recibo ya ha sido ingresado con anterioridad');
+                //ModalManager.close();
+            }
+        });
+
     }
     handlerGuardar(){
+
+        var verif;
         if(document.getElementById("verificar").value === "true"){
-          var verif=true;
+          verif=true;
         }else{
-           verif=false
+           verif=false;
         }
         var data = {};
         data.id_alum=this.props.id;
@@ -56,11 +140,16 @@ class MyModal extends Component{
         data.numero =document.getElementById("recibo").value;
         data.importe =document.getElementById("importe").value;
         data.observacion =document.getElementById("obs").value;
+        data.observacion_upg =document.getElementById("obsupg").value;
         data.fecha =document.getElementById("fecha").value;
         data.validado =verif;
         data.tipo =document.getElementById("tipo").value;
-        ModalManager.close();
+        //ModalManager.close();
+        console.log("DATA:");
+        console.log(data);
+        console.log("DATA STRINGIFY:");
         console.log(JSON.stringify(data));
+
         const url = URL.url.concat('recaudaciones/new');
         //const url= 'https://api-modulocontrol.herokuapp.com/recaudaciones/new';
         fetch(url,{
@@ -70,92 +159,110 @@ class MyModal extends Component{
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data)
-                    })
-            .then(res => res.json())
-            .then(res => {
-                if (res.status) { // exito
-                    alert('Datos creados exitosamente');
-                    ModalManager.close();
-                    alert("actualizar aqui");
-                }else{
-                  alert("FALLÓ OPERACIÓN, ESPERE UN MOMENTO Y VUELVA A INTENTARLO ")
-                }
-            });
+        }).then(res => res.json())
+        .then(res => {
+            if (res.status.localeCompare("succes")) { // exito
+                alert('Datos creados exitosamente');
+                ModalManager.close();
+            }else if (res.status.localeCompare('error')) {//ERROR
+                alert("FALLÓ OPERACIÓN " + res.message);
+            }
+            else{
+                alert(res);
+            }
+        });
     }
-
 
     render(){
         let nombre = this.props.nombre;
         let codigo = this.props.codigo;
         return (
-            <Modal
-                effect={Effect.SlideFromBottom}>
-        <div className="container" id="advanced-search-form">
-
-         <form>
-             <div className="form-group">
-                 <label >Nombres y Apellidos</label>
-                 <input type="text" className="form-control" placeholder="Nombres" id="nombre" value={nombre} disabled required/>
-             </div>
-             <div className="form-group">
-                 <label >Concepto de Pago</label>
-                 <select required id ="concepto" className="form-control"/>
-             </div>
-             <div className="form-group">
-                 <label >Código</label>
-                 <input type="number" className="form-control" placeholder="Código" id="codigo" value={codigo} disabled required/>
-             </div>
-             <div className="form-group">
-                 <label>Recibo</label>
-                 <input type="number" min="0" className="form-control" placeholder="Recibo" id="recibo" required/>
-             </div>
-             <div className="form-group">
-                 <label>Importe</label>
-                 <input type="number" min="0" className="form-control" placeholder="Importe" id="importe" required/>
-             </div>
-             <div className="form-group">
-                 <label >Fecha</label>
-                 <input type="date" id="fecha" className="form-control" required/>
-             </div>
-             <div className="form-group">
-                 <label >Ubicación</label>
-                 <select required id ="ubicacion" className="form-control" >
-                   <option value="" >Seleccione Ubicación</option>
-                   <option value="1" >Físico</option>
-                   <option value="2" >Copia</option>
-                   <option value="3" >No Disponible</option>
-                 </select>
-             </div>
-             <div className="form-group">
-                 <label >Tipo</label>
-                 <select required id ="tipo" className="form-control" >
-                   <option value="" >Seleccione Tipo</option>
-                   <option value="1" >Banco</option>
-                   <option value="2" >Manual</option>
-                 </select>
-             </div>
-             <div className="form-group">
-                 <label >Verificar</label>
-                 <select required id ="verificar" className="form-control" >
-                   <option value="true" >Validado</option>
-                   <option value="false" >No Validado</option>
-                 </select>
-             </div>
-             <div className="form-group">
-                 <label >Observaciones</label>
-                 <textarea rows="2" cols="30" id="obs">
-                 </textarea>
-             </div>
-             <button type = "button" className = "btn btn-secondary" data-dismiss = "modal" onClick = {ModalManager.close}>Cerrar</button>
-             <button type = "button" className = "btn btn-primary" onClick = {this.handlerGuardar}>ENVIAR</button>
-         </form>
-        </div>
-        <script>
-          window.onload=llenarConceptos;
-        </script>
+            <Modal style={estilonuevo} effect={Effect.SlideFromBottom}>
+                <div className="container" id="advanced-search-form">
+                    <form>
+                        <div className="form-group">
+                            <label >Nombres y Apellidos</label>
+                            <input type="text" className="form-control" placeholder="Nombres" id="nombre" value={nombre} disabled required/>
+                        </div>
+                        <div className="form-group">
+                            <label >Concepto de Pago</label>
+                            <select required id ="concepto" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label >Código</label>
+                            <input type="number" className="form-control" placeholder="Código" id="codigo" value={codigo} disabled required/>
+                        </div>
+                        <div className="form-group">
+                            <label>Recibo</label>
+                            <input onBlur = {this.handleRevisar} type="number" min="0" className="form-control" placeholder="Recibo" id="recibo" required/>
+                        </div>
+                        <div className="form-group">
+                            <label>Importe</label>
+                            <input type="number" min="0" className="form-control" placeholder="Importe" id="importe" required/>
+                        </div>
+                        <div className="form-group">
+                            <label >Fecha</label>
+                            <input type="date" id="fecha" className="form-control" required/>
+                        </div>
+                        <div className="form-group">
+                            <label >Ubicación</label>
+                            <select required id ="ubicacion" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label >Tipo</label>
+                            <select required id ="tipo" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label >Verificar</label>
+                            <select required id ="verificar" className="form-control" >
+                                <option value="true" >Validado</option>
+                                <option value="false" >No Validado</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label >Observaciones:</label>
+                            <textarea rows="2" cols="30" id="obs"></textarea>
+                        </div>
+                        <div className="form-group">
+                        </div>
+                        <div className="form-group">
+                            <label >Observaciones upg:</label>
+                            <textarea rows="2" cols="30" id="obsupg"></textarea>
+                        </div>
+                        <button type = "button" className = "btn btn-primary" onClick = {this.handlerGuardar}>ENVIAR</button>
+                        <button type = "button" className = "btn btn-secondary" data-dismiss = "modal" onClick = {ModalManager.close}>Cerrar</button>
+                    </form>
+                </div>
+                <script>
+                    window.onload=llenarConceptos;
+                </script>
             </Modal>
         );
     }
+}
 
+const estilonuevo = {
+    overlay: {
+        position        : 'fixed',
+        top             : 0,
+        left            : 0,
+        right           : 0,
+        bottom          : 0,
+        zIndex          : 99999999,
+        overflow        : 'hidden',
+        perspective     :  1300,
+        backgroundColor : 'rgba(0, 0, 0, 0.3)'
+    },
+    content: {
+        position                : 'relative',
+        margin                  : '5% auto',
+        width                   : '60%',
+        border                  : '1px solid rgba(0, 0, 0, .2)',
+        background              : '#fff',
+        overflow                : 'auto',
+        borderRadius            : '4px',
+        outline                 : 'none',
+        boxShadow               : '0 5px 10px rgba(0, 0, 0, .3)',
+    }
 }
 export default MyModal;
